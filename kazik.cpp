995961f877e29b9d01594b4c94f8e899a3cf3a29
Palcalka <vovanicipo@gmail.com>
2025-10-22 19:16:44 +0300

    kazik_1.2

--- kazik.cpp
+++ kazik.cpp
@@ -1,14 +1,58 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
+
 using namespace std;
 
+void showMenu();
+void playGame();
+
 int main()
+{
+    showMenu(); 
+    return 0;
+}
+
+void showMenu()
+{
+     int choice;
+
+    while (true) {
+        cout << "\n=========================\n";
+        cout << "       CASINO MENU       \n";
+        cout << "=========================\n";
+        cout << "1. Start game\n";
+        cout << "2. Rules\n";
+        cout << "3. Exit\n";
+        cout << "=========================\n";
+
+        if (!(cin >> choice)) {  // проверяем, ввели ли число
+            cout << "Invalid input! Please enter a number.\n";
+            cin.clear(); // сбрасываем состояние ошибки
+            cin.ignore(10000, '\n');
+            continue; // возвращаемся к началу цикла
+        }
+
+        if (choice == 1)
+            playGame(); 
+        else if (choice == 2)
+            cout << "\nRule: Bet on red, green, or black and win if color matches!\n";
+        else if (choice == 3)
+        {
+            cout << "Goodbye!\n";
+            break;
+        }
+        else
+            cout << "Invalid choice!\n";
+    }
+}
+void playGame()
 {
     srand(time(NULL));
 
     int balance = 1000;
-    string pl;
+    string pl, menu;
     int price;
     string winColor;
 
@@ -16,12 +60,19 @@ int main()
     cout << "Welcome to the Casino Roulette!\n";
     cout << "Colors: red, green, black\n";
     cout << "Your starting balance: " << balance << "$\n";
+    cout << "Enter menu if you want to go menu"<< "\n";
 
     while (true)
     {
         cout << "\n--------------------------------------\n";
-        cout << "Enter price for 1 roll (0 to stop): ";
-        cin >> price;
+        cout << "Enter price for 1 roll (0 to menu ): "<< "\n";
+        cout << "Your  balance: " << balance << "$\n";
+
+        while (!(cin >> price) || price < 0) {
+        cin.clear();              // сбрасывает ошибку потока
+        cin.ignore(10000, '\n');  // пропускает "мусор" во вводе
+        cout << "Invalid input! Enter a valid number: ";
+    }
 
         if (price == 0 || balance <= 0)
             break;
@@ -65,10 +116,10 @@ int main()
             balance -= price;
         }
 
-        cout << "Current balance: " << balance << "$\n";
+      
     }
 
     cout << "\n--------------------------------------\n";
     cout << "Game Over. Final balance: " << balance << "$\n";
-    return 0;
 }
+
